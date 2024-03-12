713e7da133ca992e63e4a03dff3b8782b37aa0d7
gucio321 <gucio321@protonmail.com>
2024-03-12 09:25:21 +0100

    conf: updates from last project

--- conf.py
+++ conf.py
@@ -10,18 +10,17 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
+from priv import *
 
 project = 'FT2zXcXvX'
 copyright = '2023, Author'
-author = 'Author'
 titlepage = {
-        "title": "Laboratorium FIzyczne - Sprawozdanie",
+        "title": "Laboratorium Fizyczne - Sprawozdanie",
         "subtitle":"Tytuł pracy",
         "subtitle2":"Document Title",
         "author": author,
