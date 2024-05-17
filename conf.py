7aff9e41a9e242f6fd4feb4569624177ce657bbb
Restyled.io <commits@restyled.io>
2024-05-17 19:50:28 +0000

    Restyled by reorder-python-imports

--- conf.py
+++ conf.py
@@ -3,16 +3,14 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Path setup --------------------------------------------------------------
-
 import os
 import sys
+import time
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import time
 
 sys.path.insert(0, os.path.abspath("."))
 
